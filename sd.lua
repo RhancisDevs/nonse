45095609b6424a28f8dc48557ab3ec06dc3a991a
RhancisDevs <129867977+RhancisDevs@users.noreply.github.com>
2026-09-10 13:28:05 +0800

    Update sd.lua

--- sd.lua
+++ sd.lua
@@ -1,3 +1,4 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
 local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
 local HttpService = game:GetService("HttpService")
